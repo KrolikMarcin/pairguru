2a29e2a829a1ab00c41e45e8903b761c21fc6e00
Marcin Krolik <krolikmarcin.mk@gmail.com>
2018-11-01 00:05:48 +0100

    Move comment validator from model to validators/one_comment_validator

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,13 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :movie
   validates :body, presence: true
-  validate :only_one_comment
-
-  private
-
-  def only_one_comment
-    checked_movie = Movie.find(movie.id)
-    errors.add(:base, "You have already commented this movie") unless
-      checked_movie.comments.where(user: user).empty?
-  end
+  validates_with OneCommentValidator
 end
